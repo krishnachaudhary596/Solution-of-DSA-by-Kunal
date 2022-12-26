e790deca7d1a737a9433654fbeb0cdbf333598c4
Krishna Chaudhary <krishnachaudhary596@gmail.com>
2022-12-26 20:09:03 +0530

    Finished palindrome number

--- 02-first-java/Palindrome.java
+++ 02-first-java/Palindrome.java
@@ -2,25 +2,30 @@ import java.util.Scanner;
 
 public class Palindrome{
   public static void main(String[] args) {
+
+    // Taking User input
     Scanner in =  new Scanner(System.in);
     System.out.println("Enter a series of number");
 
+    // Making variable
     int num = in.nextInt();
     int ans = 0;
     int temp;
     temp = num;
-    
+    // char op = in.next().trim().charAt(0);
+   
+    // Doing Calculations
     while(num > 0){
       int rem = num % 10;
       num = num / 10;
       ans = ans * 10 + rem;
     }
-
-    if (temp == ans){
-      System.out.println("Number is Palindrome");
-    } 
-    else{
-      System.out.println("Number is not Palindrome");
-    }
+      // Printing Output
+      if (temp == ans){
+        System.out.println("Number is Palindrome");
+      } 
+      else{
+        System.out.println("Number is not Palindrome");
+      }
   }
 }
